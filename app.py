2d6fa824bb78abc32932a2ad9a8bacaad1062c97
amir2227 <amir.reza2823@gmail.com>
2021-07-27 17:46:41 +0430

    add login form html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
@@ -10,9 +10,15 @@ app.config['SECRET_KEY'] = "falhdfuo@!^(*#khjSA98713dnK&(#@FDJF83R3"
 def home():
     return render_template('index.html')
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@invskills.com' and form.password.data == 'admin':
+            flash('you loged in successfully', 'success')
+            return render_template(url_for('dashbord'))
+        else:
+            flash('Unsuccessful login. Please check Email and Password.', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 if __name__ == "__main__":
